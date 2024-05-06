b77b4058e1dd1b9e8aae065a9d018d4e6c7cdf4e
kunal khairnar <kunalkhairnar61@gmail.com>
2024-05-06 18:14:49 +0530

    Doing Network calls and andling response the right way sending a http put request for all update the cart item and remove the cart item in the application

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
 
 function App() {
     const isVisible = useSelector(state => state.ui.isvisible)
+    const cart = useSelector(state => state.cart)
+  console.log(cart)
+    useEffect(()=>{
+        fetch('https://react-http-30a1e-default-rtdb.firebaseio.com/cart.json',{
+          method:'PUT',
+          body:JSON.stringify(cart),
+        });
+    },[cart])
+
   return (
     <Layout>
      {isVisible && <Cart />}
