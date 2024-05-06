c5ed7cf796e8223a1b0247c937c7a37f876050ab
kunal khairnar <kunalkhairnar61@gmail.com>
2024-05-07 01:22:47 +0530

    Fixing the Cart problems When the user reloads the page , he is not able to see the items that he saved in the cart. Everything is lost and its a nightmare for your users.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Products from './components/Shop/Products';
 import { useEffect } from 'react';
 import Notification from './components/UI/Notification';
 import { uiActions } from './Store/UiSlice/ui-slice';
+import { fetchCartData, sendCartData } from './Store/CartSlice/cart-slice';
 
 let isInitial = true;
 
@@ -13,50 +14,25 @@ function App() {
     const dispatch = useDispatch()
     const isVisible = useSelector(state => state.ui.isvisible)
     const cart = useSelector(state => state.cart)
-    
+
     //if update the state on redux reducer then notifiy update and retriwe data on the anywere of the components using useselector
     const notification = useSelector(state => state.ui.notification)
     console.log(cart)
 
+    //Geting all data inside the application percistantly
+    useEffect(()=>{
+      dispatch(fetchCartData())
+    },[dispatch])
+
     //when trigger addtocart handler or other increase , decrease Qunatity handler triggers then update the state from the reducer then if the cart have item then useEffect 
     // hook will execute and sent http request and put the cart data on http cart api from the frontend to backennd if the cart dependancy update then http put request will send on the firebase backend api call
     useEffect(()=>{
-      const sentCartData =async()=>{
-        dispatch(uiActions.showNotification({
-          status:'pending',
-          title:'sending....',
-          message:'sending cart data!'
-        }))
-        const response = await fetch('https://react-http-30a1e-default-rtdb.firebaseio.com/cart.json',{
-            method:'PUT',
-            body:JSON.stringify(cart),
-          });
+     if(isInitial){
+      isInitial = false;
+      return
+     }
 
-          if(!response.ok){
-            throw new Error('sending cart data failed')
-            
-          }
-
-          const responseData = await response.json()
-          //handling the error ui if the sucessfuly data send then update the error and then get notify on the screen
-          dispatch(uiActions.showNotification({
-            status:'success',
-            title:'Success!',
-            message:'sending cart data suceesfuly!'
-          }))
-      }
-      if(isInitial){
-        isInitial = false;
-        return;
-      }
-      //if the error then update the reducer redux state and update the notification and reflect changes on the screen
-      sentCartData().catch(error =>
-        dispatch(uiActions.showNotification({
-          status:'error',
-          title:'Error!',
-          message:'sending cart data failed!'
-        }))
-       )
+     dispatch(sendCartData(cart))
     },[cart,dispatch])
 
   return (
--- src/Store/CartSlice/cart-slice.js
+++ src/Store/CartSlice/cart-slice.js
@@ -1,4 +1,5 @@
 import {createSlice} from '@reduxjs/toolkit'
+import { uiActions } from '../UiSlice/ui-slice'
 
 const initialCartState = {
     items:[],
@@ -14,6 +15,7 @@ const cartSlice =createSlice({
             state.totalQuantity = action.payload.totalQuantity;
             state.items = action.payload.items;
           },
+
         addItemToCart(state, action) {
             console.log('EXECUTE')
             const newItem = action.payload;
@@ -32,6 +34,7 @@ const cartSlice =createSlice({
               existingItem.totalPrice = existingItem.totalPrice + newItem.price;
             }
           },
+          
           removeItemFromCart(state, action) {
             const id = action.payload;
             const existingItem = state.items.find((item) => item.id === id);
@@ -45,6 +48,86 @@ const cartSlice =createSlice({
           },
         },
 })
+
+//create a action creator for executing the api call they will help to reduce the code reusabilit and update the cart itmems manages cartdata in the application using http request
+export const sendCartData =(cart)=>{
+
+  return async(dispatch) =>{
+  
+    dispatch(uiActions.showNotification({
+      status:'pending',
+      title:'sending....',
+      message:'sending cart data!'
+    }));
+
+    const sendRequest =async()=>{
+      //this put request handle all request put post delete and update the cart handler render from the cart updates
+      const response = await fetch('https://react-http-30a1e-default-rtdb.firebaseio.com/cart.json',{
+            method:'PUT',
+            body:JSON.stringify(cart),
+          });
+
+          if(!response.ok){
+            throw new Error('sending cart data failed')
+          }
+          const responseData = await response.json()
+          console.log(responseData)
+    }
+    try {
+      await sendRequest();
+        //handling the error ui if the sucessfuly data send then update the error and then get notify on the screen
+        dispatch(uiActions.showNotification({
+          status:'success',
+          title:'Success!',
+          message:'sending cart data suceesfuly!'
+        }))
+    } catch (error) {
+      //if the error then update the throgh the dispatch to the reducer redux state and update the notification and reflect changes on the screen
+      dispatch(uiActions.showNotification({
+        status:'error',
+        title:'Error!',
+        message:'sending cart data failed!'
+      }))
+    }
+  }
+}
+
+//Geting cart data inside centralized reflect to the screen percistantly
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        'https://react-http-30a1e-default-rtdb.firebaseio.com/cart.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('Could not fetch cart data!');
+      }
+
+      const data = await response.json();
+
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Fetching cart data failed!',
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 export default cartSlice.reducer;
